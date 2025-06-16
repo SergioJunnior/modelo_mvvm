2722b080f308123fe8123fb64bad59b76fc4b2fb
Sergio Junior <sergio.ribeiro@fteam.dev>
2025-06-16 20:38:09 -0300

    feat: ajudst build. gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
+    
 }
 
 android {
     namespace = "com.example.modelo_mvvm"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,7 +26,7 @@ android {
         applicationId = "com.example.modelo_mvvm"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
